32a1e0418d19753d5515bef8d18003d2faeace63
AvinashArularasu <avinasharularasu@gmail.com>
2025-07-23 22:46:55 +0530

    resumeStart

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -11,6 +11,7 @@ export function Welcome() {
         bgHoverColorTheme,
         borderColorTheme, 
         setLoadingTrue } = UseAppContext();
+        
     const navigate = useNavigate();
 
     useEffect(() =>{
